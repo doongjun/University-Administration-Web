5dd9ff6625bf6641b89d568f189d8a80a2f84023
Kyoungju Baek <46726709+100race@users.noreply.github.com>
2021-09-16 09:48:59 +0900

    lecture logig IN PROGRESS

---
+++ src/main/java/kr/co/metanet/university/domain/LectureVO.java
@@ -0,0 +1,30 @@
+package kr.co.metanet.university.domain;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class LectureVO {
+	private int id;
+	private String lecture_code;
+	private String lecture_name;
+	private int grade;
+	private String classroom;
+	private String division;
+	private String lecture_time;
+	private String semester;
+	private int student_count;
+	private int credit;
+	private String remarks;
+	private String schedule;
+}
