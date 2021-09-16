4f7f471cd751c2310b974e20b83ffaad38b8a14e
Kyoungju Baek <46726709+100race@users.noreply.github.com>
2021-09-16 09:48:59 +0900

    lecture logig IN PROGRESS

---
+++ src/main/java/kr/co/metanet/university/controller/LectureController.java
@@ -0,0 +1,8 @@
+package kr.co.metanet.university.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller 
+public class LectureController {
+
+}
---
+++ src/main/java/kr/co/metanet/university/mapper/LectureMapper.java
@@ -0,0 +1,16 @@
+package kr.co.metanet.university.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.co.metanet.university.domain.LectureVO;
+
+@Mapper
+public interface LectureMapper {
+	List<LectureVO> getAllLecture();
+	LectureVO getLecture();
+//	int create(LectureVO vo);
+//	int update(LectureVO vo);
+	
+}
