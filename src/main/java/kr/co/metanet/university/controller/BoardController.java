a0010e74bd4ae0bab14a2e61a5067db84957c7f4
nahyunoh <82365879+nahyunoh@users.noreply.github.com>
2021-09-18 18:05:17 +0900

    게시판 list 조회

--- src/main/java/kr/co/metanet/university/controller/BoardController.java
+++ src/main/java/kr/co/metanet/university/controller/BoardController.java
@@ -1,15 +1,30 @@
 package kr.co.metanet.university.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/board/*")
+import kr.co.metanet.university.domain.BoardVO;
+import kr.co.metanet.university.service.BoardService;
+
+
+@RequestMapping(path="/board")
 @Controller
 public class BoardController {
 	
+	@Autowired
+	private BoardService service;
+	
+	
 	@GetMapping("/list")
-	private String boardlist() {
-		return "";
+	private void boardlist(Model model) {
+		List<BoardVO> list=service.boardlist();
+		model.addAttribute("list",list);
+		
+
 	}
 }
--- src/main/java/kr/co/metanet/university/service/impl/BoardServiceImpl.java
+++ src/main/java/kr/co/metanet/university/service/impl/BoardServiceImpl.java
@@ -1,7 +1,24 @@
 package kr.co.metanet.university.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.metanet.university.domain.BoardVO;
+import kr.co.metanet.university.mapper.BoardMapper;
 import kr.co.metanet.university.service.BoardService;
 
+@Service
 public class BoardServiceImpl implements BoardService {
+
+	@Autowired
+	private BoardMapper mapper;
+	
+	@Override
+	public List<BoardVO> boardlist() {
+		
+		return mapper.list();
+	}
 
 }
