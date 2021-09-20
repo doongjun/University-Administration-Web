377a2899b375e62798742a5bea4f334a1b24ae30
unknown <yey6687@daum.net>
2021-09-21 06:16:20 +0900

    MemberController 수정 및 수강->수강내역조회 기능 구현

---
+++ src/main/java/kr/co/metanet/university/service/StudentLectureService.java
@@ -0,0 +1,9 @@
+package kr.co.metanet.university.service;
+
+import java.util.List;
+
+import kr.co.metanet.university.domain.LectureVO;
+
+public interface StudentLectureService {
+	public List<LectureVO> getStudentLectureList();
+}
