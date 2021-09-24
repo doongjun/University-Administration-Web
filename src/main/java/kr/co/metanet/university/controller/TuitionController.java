b672324cf60775f4e3010eeb6689127e96e922a4
doongjun <doongjun@kakao.com>
2021-09-24 20:36:10 +0900

    마이페이지, 비밀번호 변경 폼 Validation 추가

--- src/main/java/kr/co/metanet/university/controller/TuitionController.java
+++ src/main/java/kr/co/metanet/university/controller/TuitionController.java
@@ -11,13 +11,11 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import kr.co.metanet.university.domain.Member;
 import kr.co.metanet.university.domain.Tuition;
 import kr.co.metanet.university.service.MemberService;
 import kr.co.metanet.university.service.TuitionService;
