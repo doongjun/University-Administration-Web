2c21043f3ca6dbcdad1fc4dd14ac1acd9c3ab815
nahyunoh <82365879+nahyunoh@users.noreply.github.com>
2021-09-27 17:57:41 +0900

    중간커밋

--- src/main/webapp/resources/vendor/ckeditor/config.js
+++ src/main/webapp/resources/vendor/ckeditor/config.js
@@ -35,4 +35,8 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+	//fileUpload
+	config.filebrowserUploadUrl      = '/upload.do?type=Files',
+	config.filebrowserImageUploadUrl = '/upload.do?type=Images',
+	config.filebrowserUploadMethod='form'; //파일 오류났을때 alert띄워줌
 };
