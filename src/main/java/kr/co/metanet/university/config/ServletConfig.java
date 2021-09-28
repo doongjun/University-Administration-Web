8ba5d843174189488e22a727ef446a976f133854
doongjun <doongjun@kakao.com>
2021-09-28 17:28:13 +0900

    디자인 수정

--- src/main/java/kr/co/metanet/university/config/ServletConfig.java
+++ src/main/java/kr/co/metanet/university/config/ServletConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
